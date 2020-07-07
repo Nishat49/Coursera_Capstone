002db205232e747b5626e598713fad28a4da5453
Nishat49 <67905009+Nishat49@users.noreply.github.com>
2020-07-07 02:31:51 -0700

    Add files via upload

---
+++ Coursera capstone Project.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# This book will be used for Capstone Project
+# 
+
+# In[6]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[8]:
+
+
+print('Hello Capstone project Coursera')
+
